34c4a9119693fa382c8a6ed844f6cc631882c077
Irshad Ahmad <luckyirsh786@gmail.com>
2023-03-11 01:01:21 +0530

    commit

---
+++ src/Mentors.js
@@ -0,0 +1,44 @@
+const Mentors = [
+    {
+        id: 1,
+        name: "Prankur Gupta",
+        image: "./images/Prankur.png",
+        role: "SDE @Amazon",
+        rating: "4.9",
+        description: "I am Prankur Gupta, I am working as a Software Development Engineer at Amazon. I can guide you throughout your preparation phase. Trust me when I tell you this I know the exact recipe to crack the coding interview rounds of top companies for the SDE role."
+    },
+    {
+        id: 2,
+        name: "Saurav Pal",
+        image: "./images/Saurav.png",
+        role: "SDE-2 @Amazon",
+        rating: "5.0",
+        description: "I am a Senior Software Developer American Express. Have been passionately solving Leetcode, HackerRank, Codechef, Codeforces problems, and enthusiastically solving problems with the analysis of space."
+    },
+    {
+        id: 3,
+        name: "Saumya Singh",
+        image: "./images/Saumya.png",
+        role: "Software Engineer @Red Hat",
+        rating: "5.0",
+        description: "Engineer @RedHat | Program Manager'20 @GirlScript | GHCI Scholar | International Open Source Award finalist by Red Hat | Winner SIH, 21U21 Award | Google Connect Winner'19 | Mentor GCI"
+    },
+    {
+        id: 4,
+        name: "Keshav Bathla",
+        image: "./images/Keshav.png",
+        role: "SDE-1 @Groww",
+        rating: "5.0",
+        description: "A curious learner, on a way to become a kickass developer who writes Python, Java, Javascript and Go | Software Engineer @Groww | Ex-Software Engineer @Grofers | Tech Consultant | Freelancer | Open Source Developer | Coding Instructor"
+    },
+    {
+        id: 5,
+        name: "Ankita",
+        image: "./images/Ankita.png",
+        role: "ML Engineer @Firework",
+        rating: "5.0",
+        description: "Working my way to build the world's future product. Machine Learning Engineer, Data and Technology Evangelist I breathe in developing software with intelligence. I have been involved with startups like DailyHunt, Firework Hq, MagilHub to build the AI end of their products. "
+    }
+];
+
+export default Mentors;
